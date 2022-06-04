875f642159f2b9b7d019d288ec9049338b46f00a
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2022-06-05 01:52:30 +0300

    Added AuthController

---
+++ Meter/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Meter.Controllers;
+
+public class AuthController : ControllerBase
+{
+    [Route("auth/google/redirect")]
+    [HttpGet]
+    public RedirectResult GoogleRedirect()
+    {
+        return Redirect("");
+    }
+
+    [Route("auth/google/callback")]
+    [HttpGet]
+    public string GoogleCallback()
+    {
+        return "Google callback";
+    }
+}
