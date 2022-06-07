53304c17275444fd4531f37f8d984041da86ba9e
Anna Romaniuk <80681158+AnechkaR@users.noreply.github.com>
2022-06-07 16:36:31 +0300

    Created maps for requests

--- Meter/Mapping/AutoMapperProfiles.cs
+++ Meter/Mapping/AutoMapperProfiles.cs
@@ -1,7 +1,10 @@
 ﻿using AutoMapper;
 using Meter.Dtos;
 using Meter.Models;
+using Meter.Requests.Counter;
 using Meter.Requests.Group;
+using Meter.Requests.Icon;
+using Meter.Requests.Measure;
 
 namespace Meter.Mapping;
 
@@ -17,13 +20,24 @@ public class AutoMapperProfiles : Profile
         CreateMap<GroupCreateRequest, Group>();
         CreateMap<GroupUpdateRequest, Group>()
             .ForMember(c => c.Counters, option => option.Ignore())
-            .ForMember(c => c.Owner, option => option.Ignore());;
+            .ForMember(c => c.Owner, option => option.Ignore());
         
         CreateMap<Counter, CounterDto>();
+        CreateMap<CounterCreateRequest, Counter>();
+        CreateMap<CounterUpdateRequest, Counter>()
+            .ForMember(c => c.Group, option => option.Ignore())
+            .ForMember(c => c.Icon, option => option.Ignore())
+            .ForMember(c => c.Measures, option => option.Ignore());
         
         
         CreateMap<Measure, MeasureDto>();
+        CreateMap<MeasureCreateRequest, Measure>();
+        CreateMap<MeasureUpdateRequest, Measure>()
+            .ForMember(c => c.Counter, option => option.Ignore());
         
         CreateMap<Icon, IconDto>();
+        CreateMap<IconCreateRequest, Icon>();
+        CreateMap<IconUpdateRequest, Icon>()
+            .ForMember(c => c.Counters, option => option.Ignore());
     }
 }
