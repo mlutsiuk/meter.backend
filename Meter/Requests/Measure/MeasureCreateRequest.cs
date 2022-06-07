e39e9a0b80d12b180174be553e1c9564bec5ecbe
Anna Romaniuk <80681158+AnechkaR@users.noreply.github.com>
2022-06-07 16:28:13 +0300

    Created requests for measure

---
+++ Meter/Requests/Measure/MeasureCreateRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Meter.Requests.Measure;
+
+public class MeasureCreateRequest
+{
+    [Required]
+    public DateTime Date { get; set; }
+    [Required]
+    public int Value { get; set; }
+    
+    [Required]
+    public int CounterId { get; set; }
+}
