6efe28a1559a836d992d3365e54698c2199627a6
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2022-06-11 22:34:38 +0300

    Enabled automatic migrations. Added Azure database to data sources.

--- Meter/Program.cs
+++ Meter/Program.cs
@@ -75,4 +75,12 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<AppDbContext>();    
+    context.Database.Migrate();
+}
+
 app.Run();
